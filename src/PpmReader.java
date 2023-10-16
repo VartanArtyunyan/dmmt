14f8ada0fe55e82cfeedff9ad23267952e78128d
Vartan Artyunyan <vartan.artyunyan@study.thwd.de>
2023-10-16 17:14:43 +0200

    Added PPM reader

--- src/PpmReader.java
+++ src/PpmReader.java
@@ -1,2 +1,38 @@
+import models.Picture;
+import models.RGBPixel;
+
+
 public class PpmReader {
+
+
+    public static Picture<RGBPixel> convertPPMStringToPicture(String ppm) {
+        // Split the input string into lines
+        String[] lines = ppm.split("\\n");
+
+
+        int width = lines[0].split(" ").length / 3;
+        int height = lines.length;
+
+        Picture.PictureBuilder<RGBPixel> builder = new Picture.PictureBuilder<>(width, height, RGBPixel.class);
+
+        RGBPixel[][] pixels =  new RGBPixel[width][height];
+
+        // Iterate over the rest of the lines to get pixel data
+        int pixelDataStartIndex = 3; // Adjust if more header lines
+        for (int y = 0; y < height; y++) {
+            String[] rgbValues = lines[pixelDataStartIndex + y].split(" ");
+            for (int x = 0; x < width; x++) {
+                // Extract RGB values from the string
+                double r = Double.parseDouble(rgbValues[x * 3]);
+                double g = Double.parseDouble(rgbValues[x * 3 + 1]);
+                double b = Double.parseDouble(rgbValues[x * 3 + 2]);
+
+                // Create a new RGBPixel and set it in the Picture object
+                pixels[x][y] = new RGBPixel(r, g, b);
+            }
+        }
+
+        return builder.withPixels(pixels).build();
+    }
+
 }
--- src/models/Picture.java
+++ src/models/Picture.java
@@ -8,20 +8,10 @@ public class Picture<T extends Pixel> {
     private final int realX;
     private final int realY;
 
-    public Picture(int realX, int realY, Class<T> pixelClass) {
+    private Picture(int realX, int realY, T[][] pixels) {
         this.realX = realX;
         this.realY = realY;
-        int x;
-        int y;
-        if (realX % 16 == 0) {
-            x = realX;
-        } else {
-            x = 16 - (realX % 16) + realX;
-        }
-        if (realY % 16 == 0) {
-            y = realY;
-        } else y = 16 - (realX % 16) + realY;
-        pixels = (T[][]) Array.newInstance(pixelClass, x, y);
+       this.pixels = pixels;
     }
 
     public Pixel[][] getPixels() {
@@ -54,5 +44,76 @@ public class Picture<T extends Pixel> {
 
     public int[][] getThirdChannel() {
         return (int[][]) Arrays.stream(pixels).map(ts -> Arrays.stream(ts).map(Pixel::getThird).toArray()).toArray();
+    }
+
+
+
+    public static class PictureBuilder <T extends Pixel> {
+
+        private T[][] pixels;
+
+        private int realX;
+
+        private int realY;
+
+        private int lastRealXCoord;
+
+        private int lastRealYCoord;
+
+        public PictureBuilder (int realX, int realY, Class<T> pixelClass) {
+            this.realX = realX;
+            this.realY = realY;
+            lastRealXCoord = realX - 1;
+            lastRealYCoord = realY - 1;
+            int x;
+            int y;
+            if (realX % 16 == 0) {
+                x = realX;
+            } else {
+                x = 16 - (realX % 16) + realX;
+            }
+            if (realY % 16 == 0) {
+                y = realY;
+            } else y = 16 - (realX % 16) + realY;
+
+            pixels = (T[][]) Array.newInstance(pixelClass, x, y);
+        }
+
+        public PictureBuilder withPixels(T[][] pixels){
+            for(int x = 0; x < pixels.length; x++){
+                for(int y = 0; y < pixels.length; y++){
+                    this.pixels[x][y] = pixels[x][y];
+                }
+            }
+            this.pixels = pixels;
+            return this;
+        }
+
+        public Picture build(){
+
+
+            for(int x = realX; x < pixels.length; x++){
+                for(int y = 0; y < realY; y++){
+                   pixels[x][y] = pixels[lastRealXCoord][y];
+                }
+            }
+
+            for(int x = 0; x < realX; x++){
+                for(int y = realY; y > pixels[x].length; y++){
+                  pixels[x][y] = pixels[x][lastRealYCoord];
+                }
+            }
+
+            for(int x = realX; x < pixels.length; x++){
+                for(int y = realY; y < pixels.length; y++){
+                    pixels[x][y] = pixels[lastRealXCoord][lastRealYCoord];
+                }
+            }
+
+
+            return new Picture(realX, realY, pixels);
+        }
+
+
     }
 }
